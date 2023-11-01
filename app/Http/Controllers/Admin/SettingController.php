6ce5979c8bafd0dc11f2e44884ec7940c447aaf1
Hoang Mach It <hoangmach.website@gmail.com>
2023-11-01 23:48:28 +0700

    Setting

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -5,19 +5,32 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Language;
 use App\Models\Setting;
+use App\Repositories\Language\LanguageRepositoryInterface;
+use App\Repositories\Setting\SettingRepositoryInterface;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class SettingController extends Controller
 {
     /**
+     * Summary of __construct
+     * @param SettingRepositoryInterface $settingRepository
+     * @param LanguageRepositoryInterface $languageRepository
+     */
+    public function __construct(
+        private SettingRepositoryInterface $settingRepository,
+        private LanguageRepositoryInterface $languageRepository
+    ) {
+    }
+
+    /**
      * Summary of index
-     * @return \Inertia\Response
+     * @return Response
      */
     public function index(): Response
     {
-        $setting = Setting::first();
-        $languages = Language::all();
+        $setting = $this->settingRepository->first();
+        $languages = $this->languageRepository->getAll();
         return Inertia::render('Admin/Setting/Index', [
             'setting' => $setting,
             'languages' => $languages,
